f1ba5f45dda5450c009d6728387d3d16b2d613c1
kenchaso <kenchasonakai@gmail.com>
2023-03-30 15:34:02 +0900

    add: actioncable機能の作成

---
+++ app/javascript/channels/room_channel.js
@@ -0,0 +1,28 @@
+import consumer from "channels/consumer"
+
+const appRoom = consumer.subscriptions.create("RoomChannel", {
+  connected() {
+    // Called when the subscription is ready for use on the server
+  },
+
+  disconnected() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received(data) {
+    const messages = document.getElementById('messages');
+    messages.insertAdjacentHTML('beforeend', data['message']);
+  },
+
+  speak: function(message) {
+    return this.perform('speak', { message: message });
+  }
+});
+
+window.document.onkeydown = function(event) {
+  if(event.key == 'Enter') {
+    appRoom.speak(event.target.value);
+    event.target.value = '';
+    event.preventDefault();
+  }
+}
